fd1413c7448d5fc2287638d2ab17ec6cee865d46
Irving Ventura <irvingv8@gmail.com>
2016-10-03 00:17:50 -0400

    styling video list

--- src/index.js
+++ src/index.js
@@ -1,28 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
-import SearchBar from './components/search_bar'	
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
-const API_KEY = 'AIzaSyCrqhSOJNI-36Wot2CT6jBRXtp72rcJtIk'
+const API_KEY = 'AIzaSyCrqhSOJNI-36Wot2CT6jBRXtp72rcJtIk';
 
 // Create a new Component. This Component should produce some html
 
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
-}
+class App extends Component {
+	constructor (props) {
+		super();
 
-//==============================================================================
-// This down here is the same as the code right above
-//==============================================================================
-// const App = function() {
-// 	return (
-// 		<div> Mortal! You are not prepared. </div>
-// 	);
-// }
+		this.state = {
+			videos: []
+		};
+
+		YTSearch({ key: API_KEY, term: 'dota' }, (videos) => {
+
+			// this really means "this.setState({videos : videos});"
+			// when the property and the parameter has the same name this can be
+			// wrote as bellow
+			this.setState({videos});
+
+		});
+	}
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				<VideoList videos={this.state.videos}/>
+			</div>
+		);
+	}
+}
 
 // take this Component's generated html and put it in the DOM
 ReactDOM.render(<App></App>, document.querySelector('#container'));
