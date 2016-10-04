7dd4e1b84cff4b5558cf6afdacebe6eb6ee27b2b
Irving Ventura <irvingv8@gmail.com>
2016-10-04 00:26:01 -0400

    finished app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
+import _ from 'lodash'
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
@@ -19,7 +20,11 @@ class App extends Component {
 			selectedVideo: null
 		};
 
-		YTSearch({ key: API_KEY, term: 'dota' }, (videos) => {
+		this.videoSearch('dota 2');
+	}
+
+	videoSearch(term) {
+		YTSearch({ key: API_KEY, term: term }, (videos) => {
 
 			// this really means "this.setState({videos : videos});"
 			// when the property and the parameter has the same name this can be
@@ -30,11 +35,14 @@ class App extends Component {
 			});
 
 		});
-	}
+	};
+
 	render() {
+		const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300)
+
 		return (
 			<div>
-				<SearchBar />
+				<SearchBar onSearchTermChange = {videoSearch}/>
 				<VideoDetail video={this.state.selectedVideo}/>
 				<VideoList
 					onVideoSelect={(selectedVideo) => this.setState({selectedVideo})}
